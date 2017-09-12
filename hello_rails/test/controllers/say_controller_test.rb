5a7f8c85320896e242c442a96c56ea33226b6efc
Vikram Krishnan <vikram.krishnan@careem.com>
2017-09-12 20:25:32 +0530

    "Changed controller and controller test"

--- hello_rails/test/controllers/say_controller_test.rb
+++ hello_rails/test/controllers/say_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
-class SayControllerTest < ActionDispatch::IntegrationTest
+#class SayControllerTest < ActionDispatch::IntegrationTest
+class SayControllerTest < ActionController::TestCase
   test "should get hello" do
     #get say_hello_url
     get :hello
